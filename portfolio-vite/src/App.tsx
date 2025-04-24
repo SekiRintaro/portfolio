22eae9dddfe15fa4041fd076e7fef8a23c80e701
rintaro seki <sekimametaro@gmail.com>
2025-04-25 05:04:38 +0900

    mv vite

---
+++ portfolio-vite/src/App.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import Header from '../../portfolio-vite/src/components/Header';
+import Footer from '../../portfolio-vite/src/components/Footer';
+import Home from '../../portfolio-vite/src/pages/Home';
+import Loader from '../../portfolio-vite/src/components/Loader';
+import Contact from '../../portfolio-vite/src/pages/Contact';
+import About from '../../portfolio-vite/src/pages/About';
+
+const App: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+
+  const handleFinishLoading = () => {
+    setLoading(false);
+  };
+
+  return (
+    <>
+      {loading ? (
+        <Loader onFinish={handleFinishLoading} />
+      ) : (
+        <div>
+          <div id="top"></div>
+          <Header />
+          <main>
+            <section id="home" className="h-screen bg-blue-100">
+              <Home />
+            </section>
+            <section id="about" className="bg-[#f5f1e4]">
+              <About />
+            </section>
+            <section id="contact" className="bg-[#f5f1e4]">
+              <Contact />
+            </section>
+          </main>
+          <Footer />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default App;
---
+++ portfolio-vite/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="bg-secondary text-white py-4 px-8 text-center">
+            <p>&copy; Seki Rintaro 2025</p>
+        </footer>
+    );
+};
+
+export default Footer;
